87ee2b89e24e7c32205282cdfadc83b8bef40d14
Douglas Franca <douglasdf2809@gmail.com>
2024-08-01 00:23:51 -0300

    Fix

--- exec.sh
+++ exec.sh
@@ -7,7 +7,12 @@ python3 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
-
 pip install gunicorn
+
+# Check if port 8000 is in use and kill the process if necessary
+if lsof -i:8000 -t >/dev/null; then
+    echo "Port 8000 is in use. Stopping the process using it."
+    kill -9 $(lsof -i:8000 -t)
+fi
 
 gunicorn -w 4 -b 0.0.0.0:8000 wsgi:app
